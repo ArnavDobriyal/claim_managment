bedbb76c46afab40c0707ebf05f683b5ab75c0fe
ARNAV <arnavdobriyalofficial@gmail.com>
2025-02-04 13:04:48 +0530

    Added database

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql+asyncpg://postgres:arnav1234@localhost:5432/postgres"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://USER:PASSWORD@HOST:PORT/DATABASE")
 
-engine = create_async_engine(DATABASE_URL, future=True, echo=True)
-AsyncSessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
-Base = declarative_base()
+engine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
